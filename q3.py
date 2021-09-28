3b40f72ceaa501d493b45d31d620930e03266364
Gabriel Dinse <gabriel.dinse@outlook.com>
2021-09-27 21:58:26 -0300

    Added q3

--- q3.py
+++ q3.py
@@ -1,20 +1,37 @@
-def caminhao(v, w, C):
+def caminhao(v, w, C, delivered):
     N = len(v)
-    M = {}
-    for i in range(C+1):
-        M[(0,i)] = 0
-    for i in range(1, N+1):
-        for j in range(C+1):
-            if j < w[i-1]:
-                M[(i, j)] = M[(i-1, j)]
-            else:
-                M[(i, j)] = max(M[(i-1, j)], v[i-1] + M[(i-1, j-w[i-1])])
-    return M[(N,C)]
+    M = [[0 for i in range(C+1)] for j in range(N+1)] 
+    for i in range(1, N):
+        if not delivered[i]:
+            for j in range(C+1):
+                if j < w[i]:
+                    M[i][j] = M[i-1][j]
+                else:
+                    M[i][j] = max(M[i-1][j], v[i] + M[i-1][j-w[i]])
+    total = M[N][C]
+    remaining_w = C
+    items = [] 
+
+    for i in reversed(range(1, len(v))):
+        if not delivered[i]:
+            if M[i][remaining_w] == M[i - 1][remaining_w] + v[i]:
+               items.append(i)
+               remaining_w -= w[i]
+    return items
+
+   # return M[(N,C)]
 
 T = ['t1', 't2', 't3']                                            #T: Conjunto de caminhões.
-c = [2000, 4000, 3000]                                            #t: Capacidade de carga dos caminhões em kg
-G = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10'] #G: Conjunto de itens.
-w = [500, 300, 750, 650, 250, 200, 800, 400, 100, 550]            #w: Peso dos intens em kg.
-v = [3000, 1500, 400, 5000, 10000, 200, 300, 2000, 4100, 6100]    #w: Lucro na entrega em reais de cada item.
+C = [2000, 4000, 3000]                                            #t: Capacidade de carga dos caminhões em kg
+G = [None, 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10'] #G: Conjunto de itens.
+w = [None, 11500, 1300, 11750, 111650, 1250, 11200, 1800, 1400, 1100, 1550]            #w: Peso dos intens em kg.
+v = [None, 3000, 1500, 400, 5000, 10000, 200, 300, 2000, 4100, 6100]    #w: Lucro na entrega em reais de cada item.
+delivered = [False] * len(v)
 
-print('Soma do lucro dos itens entregues:',caminhao(v, w, c[1]))
+for c in C:
+    items = caminhao(v, w, c, delivered)
+    for item in items:
+        delivered[item] = True
+
+print(f'Itens não entregues: {[g for i, g in enumerate(G[1:]) if not delivered[i]]}')
+print(f'Valor não entregue: {sum([value for i, value in enumerate(v[1:]) if not delivered[i]])}')
